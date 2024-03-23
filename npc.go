4d674a1927157294de9213884c512f913248b6ac
nvm <s.avkhadeev@gmail.com>
2024-03-23 22:27:13 +0500

    update spawn point logic

--- npc.go
+++ npc.go
@@ -7,25 +7,36 @@ import (
 
 var npcTanks []*Tank
 
-func getSpawnPoints() [][2]int {
-	return [][2]int{
-		{BoxLeft + 1, BoxTop + 1},
-		{BoxRight - 6, BoxTop + 1},
-		{BoxLeft + 1, BoxBottom - 3},
-		{BoxRight - 6, BoxBottom - 3},
+var spawnPoints = [][2]int{
+	{BoxLeft + 1, BoxTop + 1},
+	{BoxRight - 6, BoxTop + 1},
+	{BoxLeft + 1, BoxBottom - 3},
+	{BoxRight - 6, BoxBottom - 3},
+}
+
+func getSpawnPoint() (int, int, bool) {
+	if len(spawnPoints) == 0 {
+		return 0, 0, false
 	}
+
+	randomIndex := rand.Intn(len(spawnPoints))
+	spawnPoint := spawnPoints[randomIndex]
+	spawnPoints = append(spawnPoints[:randomIndex], spawnPoints[randomIndex+1:]...)
+
+	return spawnPoint[0], spawnPoint[1], true
 }
 
 func spawn(box *Box) {
 	ticker := time.NewTicker(1 * time.Second)
-	spawnPoints := getSpawnPoints()
 
 	for range ticker.C {
 		if len(npcTanks) < 4 {
-			spawnPoint := spawnPoints[rand.Intn(len(spawnPoints))]
-			npcTank := NewTank(spawnPoint[0], spawnPoint[1])
-			npcTank.Draw(box.Screen)
-			npcTanks = append(npcTanks, npcTank)
+			x, y, ok := getSpawnPoint()
+			if ok {
+				npcTank := NewTank(x, y)
+				npcTank.Draw(box.Screen)
+				npcTanks = append(npcTanks, npcTank)
+			}
 		}
 	}
 }
