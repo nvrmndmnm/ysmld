fa37331f33a62432628676eae8888c9cbce18dc3
nvm <s.avkhadeev@gmail.com>
2024-04-16 15:28:47 +0500

    refactor ammo handling

--- projectile.go
+++ projectile.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gdamore/tcell/v2"
+import (
+	"github.com/gdamore/tcell/v2"
+	"time"
+)
 
 type Projectile struct {
 	Object
@@ -57,5 +60,49 @@ func (p *Projectile) Move(box *Box) {
 
 		pixel.X += dx
 		pixel.Y += dy
+	}
+}
+
+func handleAmmo(box *Box, playerTank *Tank, projectiles chan *Projectile, quitCh chan struct{}) {
+	var ammoRack []*Projectile
+	ticker := time.NewTicker(ProjectileSpeed * time.Millisecond)
+	for {
+		select {
+		case projectile := <-projectiles:
+			ammoRack = append(ammoRack, projectile)
+		case <-ticker.C:
+			for i := len(ammoRack) - 1; i >= 0; i-- {
+				if ammoRack[i].Pixels[0].Y > BoxTop &&
+					ammoRack[i].Pixels[0].Y < BoxBottom &&
+					ammoRack[i].Pixels[0].X > BoxLeft+1 &&
+					ammoRack[i].Pixels[1].X < BoxRight-1 {
+
+					for _, npcTank := range npcTanks {
+						if isHit(npcTank, ammoRack[i]) {
+							ammoRack[i].Clear(box)
+							ammoRack = append(ammoRack[:i], ammoRack[i+1:]...)
+
+							despawn(npcTank, box)
+							break
+						}
+					}
+
+					if i < len(ammoRack) {
+						ammoRack[i].Move(box)
+						ammoRack[i].Draw(box)
+					}
+				} else {
+					ammoRack[i].Clear(box)
+					ammoRack = append(ammoRack[:i], ammoRack[i+1:]...)
+				}
+			}
+
+			playerTank.Draw(box)
+			box.Screen.Show()
+
+			case <-quitCh:
+				ticker.Stop()
+				return
+		}
 	}
 }
